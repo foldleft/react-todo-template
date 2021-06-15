9d9def68d40feb0609e351bc8dea1348e018f283
Zoe <foldleft@users.noreply.github.com>
2021-06-15 11:05:20 +0100

    A partial todo app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,78 @@
+import { useState, useCallback } from "react";
+
 import "./styles.css";
 
-export default function App() {
+/*
+Hooks reference: https://reactjs.org/docs/hooks-intro.html
+Hooks API reference: https://reactjs.org/docs/hooks-reference.html
+*/
+
+export function TodoItem({ text, complete, onClick, onDelete }) {
   return (
-    
-      see some magic happen!</h2>
+    <li>
+      <button className="checkButton" onClick={onClick}>
+        {complete ? "☑" : "☐"}
+      </button>
+      <span>{text}</span>
+      <button className="deleteButton" onClick={onDelete}>
+        ×
+      </button>
+    </li>
+  );
+}
+
+export default function App() {
+  const [items, setItems] = useState([
+    { text: "Example item", complete: false }
+  ]);
+  const [input, setInput] = useState("");
+
+  const updateItem = useCallback(
+    (index) => () => {
+      // Implement me
+    },
+    []
+  );
+
+  const deleteItem = useCallback(
+    (index) => () => {
+      const newItems = [...items];
+      newItems.splice(index, 1);
+      setItems(newItems);
+    },
+    [items]
+  );
+
+  return (
+    <div className="App">
+      <h2>Todo</h2>
+      <ul>
+        {items.map((item, i) => (
+          <TodoItem
+            {...item}
+            key={i}
+            onClick={updateItem(i)}
+            onDelete={deleteItem(i)}
+          />
+        ))}
+        <li>
+          <input
+            type="text"
+            onChange={(event) => {
+              setInput(event.target.value);
+            }}
+            onKeyUp={({ key }) => {
+              if (key === "Enter") {
+                if (input === "") return;
+                const item = { text: input, complete: false };
+                setItems([...items, item]);
+                setInput("");
+              }
+            }}
+            value={input}
+          />
+        </li>
+      </ul>
     </div>
   );
 }
